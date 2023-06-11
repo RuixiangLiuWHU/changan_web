a6407a84be77c7282354f61fb61f4ca57a3e3f76
RuixiangLiuWHU <lrx_lucky@whu.edu.cn>
2023-06-11 21:31:02 +0800

    修改

--- src/modules/apiConfig.js
+++ src/modules/apiConfig.js
@@ -1,8 +1,8 @@
 /*
  * @Author: lrx lrx_lucky@whu.edu.cn
  * @Date: 2021-09-26 15:16:45
- * @LastEditors: lrx lrx_lucky@whu.edu.cn
- * @LastEditTime: 2023-02-20 16:22:39
+ * @LastEditors: RuixiangLiuWHU lrx_lucky@whu.edu.cn
+ * @LastEditTime: 2023-06-11 21:30:31
  * @FilePath: \Changan University\src\modules\apiConfig.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,5 +11,5 @@ var isPro = process.env.NODE_ENV === 'production' //process.env.NODE_ENV用于�
 
 //根据环境不同导出不同的baseURL
 export default {
-    baseURL: isPro ? 'http://125.220.153.25:8097/changan/' : 'http://125.220.153.25:8097/changan/',
+    baseURL: isPro ? 'http://125.220.153.25:8097/changan/' : 'http://localhost:8097/changan/',
 }
